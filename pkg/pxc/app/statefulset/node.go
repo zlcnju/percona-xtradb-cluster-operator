ad19d0f259e698006f518c48832e5388f2c51756
Max Dudin <dudin.mv@gmail.com>
2020-08-21 23:00:21 +0300

    K8SPXC-368 update pmm container

--- pkg/pxc/app/statefulset/node.go
+++ pkg/pxc/app/statefulset/node.go
@@ -233,10 +233,10 @@ func (c *Node) SidecarContainers(spec *api.PodSpec, secrets string, cr *api.Perc
 }
 
 func (c *Node) PMMContainer(spec *api.PMMSpec, secrets string, cr *api.PerconaXtraDBCluster) (*corev1.Container, error) {
-	ct := app.PMMClient(spec, secrets, cr.CompareVersionWith("1.2.0") >= 0)
 	if cr.CompareVersionWith("1.6.0") >= 0 {
 		secrets = "internal-" + cr.Name
 	}
+	ct := app.PMMClient(spec, secrets, cr.CompareVersionWith("1.2.0") >= 0)
 	pmmEnvs := []corev1.EnvVar{
 		{
 			Name:  "DB_TYPE",
